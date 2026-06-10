76024efc10fc4fb4fe971c5e34eab7c3c11fe103
willsrockson <marleyenos18@gmail.com>
2026-06-10 18:58:43 +0000

    Setup thread safe idempotency store and payload hashing

--- internal/paymentservice/payment_handler.go
+++ internal/paymentservice/payment_handler.go
@@ -1,12 +1,41 @@
 package paymentservice
 
 import (
+	"crypto/sha256"
+	"encoding/hex"
 	"fmt"
+	"sync"
 
 	"github.com/labstack/echo/v5"
 )
 
+const (
+	Processing = "PROCESSING"
+	Completed  = "COMPLETED"
+)
+
+// IdempotencyRecord holds the state of a single transaction intent
+type IdempotencyRecord struct {
+	State       string
+	PayloadHash string
+	StatusCode  int
+	Response    interface{}
+	Ready       chan struct{} //channel used to block duplicate in flight requests
+}
+
+// Thread safe map to store our records
+var store = struct {
+	sync.RWMutex
+	m map[string]*IdempotencyRecord
+}{m: make(map[string]*IdempotencyRecord)}
+
 type handler struct {
+}
+
+// generate SHA-256 hash
+func hashPayload(data []byte) string {
+	hash := sha256.Sum256(data)
+	return hex.EncodeToString(hash[:])
 }
 
 func (h *handler) ProcessPayment(ctx *echo.Context) error {
